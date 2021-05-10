4121f5be2a595eff6ea0e3ff989eaf1f7a625338
awilliams <645192+awilliams@users.noreply.github.com>
2021-05-09 22:36:49 -0600

    Extract days type to separate file

---
+++ src/days.rs
@@ -0,0 +1,42 @@
+use std::slice::Iter;
+
+use chrono::prelude::*;
+use chrono::Duration;
+
+pub struct Days {
+    pub week_num: u32,
+    pub days: Vec<Date<Local>>,
+}
+
+impl Days {
+    pub fn from_offset(week_offset: i64) -> Self {
+        let monday = Self::monday(week_offset);
+
+        let mut days = Vec::with_capacity(5);
+        days.push(monday);
+        for i in 1..=4 {
+            days.push(monday + Duration::days(i));
+        }
+
+        Self {
+            week_num: monday.iso_week().week(),
+            days: days,
+        }
+    }
+
+    fn monday(week_offset: i64) -> Date<Local> {
+        let today: Date<Local> = Local::today();
+        let monday_offset = Duration::days(1 - today.weekday().num_days_from_sunday() as i64);
+        let offset = monday_offset + Duration::weeks(week_offset);
+        today + offset
+    }
+}
+
+impl<'a> IntoIterator for &'a Days {
+    type Item = &'a Date<Local>;
+    type IntoIter = Iter<'a, Date<Local>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.days.iter()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 mod cli;
+use cli::CLI;
+mod days;
+use days::Days;
 
 use std::env;
 use std::process;
 
-use std::vec::IntoIter;
-
-use chrono::prelude::*;
-use chrono::Duration;
-
 fn main() {
-    let args = cli::CLI::from_args(&mut env::args_os()).unwrap_or_else(|e| {
+    let args = CLI::from_args(&mut env::args_os()).unwrap_or_else(|e| {
         eprintln!("{}", e);
         process::exit(1);
     });
@@ -18,45 +16,10 @@ fn main() {
 
     let days = Days::from_offset(args.week_offset);
     println!("week number: {}", days.week_num);
-    for day in days {
+    for day in &days {
         println!("  {}", day);
     }
-}
-
-struct Days {
-    week_num: u32,
-    days: Vec<Date<Local>>,
-}
-
-impl Days {
-    fn from_offset(week_offset: i64) -> Self {
-        let monday = Self::monday(week_offset);
-
-        let mut days = Vec::with_capacity(5);
-        days.push(monday);
-        for i in 1..=4 {
-            days.push(monday + Duration::days(i));
-        }
-
-        Self {
-            week_num: monday.iso_week().week(),
-            days: days,
-        }
-    }
-
-    fn monday(week_offset: i64) -> Date<Local> {
-        let today: Date<Local> = Local::today();
-        let monday_offset = Duration::days(1 - today.weekday().num_days_from_sunday() as i64);
-        let offset = monday_offset + Duration::weeks(week_offset);
-        today + offset
-    }
-}
-
-impl IntoIterator for Days {
-    type Item = Date<Local>;
-    type IntoIter = IntoIter<Self::Item>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.days.into_iter()
+    for day in &days {
+        println!("  {}", day);
     }
 }
